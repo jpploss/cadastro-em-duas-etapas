6a952c29c78f18c888b27847235f1c8dae5504d4
jpploss <joao.loss@edu.ufes.br>
2023-10-26 18:01:16 -0300

    Add uns comentários

--- faceRec.py
+++ faceRec.py
@@ -2,12 +2,16 @@ import cv2 # importar o opencv -> para instalar rode pip install opencv-python
 import mediapipe as mp # para instalar rode pip install mediapipe
 
 webcam = cv2.VideoCapture(0) # para conectar o python com a nossa webcam.
+# VideoCapture: classe do OpenCV responsável por realizar a captura de frames em um vídeo. Passamos o caminho como parâmetro na criação do objeto
+# leitura da webcam, precisamos passar o seu ID de referência (em um notebook, o ID da câmera primária é 0)
+
+print(webcam)
 
 reconhecimento_rosto = mp.solutions.face_detection # ativando a solução de reconhecimento de rosto
 desenho = mp.solutions.drawing_utils # ativando a solução de desenho
 reconhecedor_rosto = reconhecimento_rosto.FaceDetection() # criando o item que consegue ler uma imagem e reconhecer os rostos ali dentro
 
-while webcam.isOpened():
+while webcam.isOpened(): 
     validacao, frame = webcam.read() # lê a imagem da webcam
     if not validacao:
         break
@@ -19,7 +23,7 @@ while webcam.isOpened():
             desenho.draw_detection(imagem, rosto) # desenha o rosto na imagem
     
     cv2.imshow("Rostos na sua webcam", imagem) # mostra a imagem da webcam para a gente
-    if cv2.waitKey(5) == 27: # ESC # garante que o código vai ser pausado ao apertar ESC (código 27) e que o código vai esperar 5 milisegundos a cada leitura da webcam
+    if cv2.waitKey(5) == 27: # garante que o código vai ser pausado ao apertar ESC (código 27) e que o código vai esperar 5 milisegundos a cada leitura da webcam
         break
 webcam.release() # encerra a conexão com a webcam
 cv2.destroyAllWindows() # fecha a janela que mostra o que a webcam está vendo

2eb75cf1d4f70b8424117b9a24fee9f81d95ee07
jpploss <joao.loss@edu.ufes.br>
2023-10-31 09:22:08 -0300

    fazendo base de dados e interagindo com ela

--- main.py
+++ main.py
@@ -1,5 +1,27 @@
 from deepface import DeepFace # resposável por realizar o validação facial
 import cv2 # resposável por realizar a manipulação da webcam
+import json # útil para lidar com a base de usuário (arquivo .json)
 
 
+with open("dataBase.json") as file_json:
+    user = json.load(file_json) # ususario recebe um objeto correspondente ao conteúdo do .json
 
+
+name = input("Digite o seu nome de usuário: ")
+password = input("Digite sua senha: ")
+
+user_list = user['users']
+logado = False
+for i in range(len(user_list)):
+    if name == user_list[i]['name']:
+        if password == user_list[i]['password']:
+            print("Bem vindo")
+            logado = True
+        else:
+            print("Nome de usuário e/ou senha incorretos")
+ 
+
+if not logado:
+    print("Nome de usuário e/ou senha incorretos")
+else:
+    

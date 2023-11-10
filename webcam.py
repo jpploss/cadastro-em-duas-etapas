2d99da335ca2d17798dbed724121f0df2cc829d0
jpploss <joao.loss@edu.ufes.br>
2023-11-10 19:38:06 -0300

    tentando começar a parte de reconhecimento facial

--- webcam.py
+++ webcam.py
@@ -1,29 +1,42 @@
 import cv2
-from deepface import DeepFace
-# video util: https://www.youtube.com/watch?v=r8Qg3NfdiHc
+import face_recognition as fr
 
-webcam = cv2.VideoCapture(0) # faz a conexão com a webcam
+def get_encodings_faces(url_img):
 
-if webcam.isOpened(): # verifica se a conexão com a webcam foi bem sucedida
-    validacao ,frame = webcam.read() # le a imagem da webcam
+    img = fr.load_image_file(url_img)
 
-    while validacao:
-        validacao ,frame = webcam.read()
-        cv2.imshow("webcam do joss", frame)
-        key = cv2.waitKey(3) # retorna um inteiro correpondendo ao código ASCII da tecla apertada
+    # faces recebe uma lista com os rostos contidos na imagem
+    faces = fr.face_encodings(img)
 
-        result = DeepFace.verify(img1_path = "h1.png", img2_path = frame)
-        if(result['verified']):
-            print("oi hen")
-            break
-        elif DeepFace.verify(img1_path = "test.png", img2_path = frame)['verified']:
-            print("oi jp")
-            break
-        
-        if key == 27:
-            break
-    
-    cv2.imwrite('test2.png', frame) # cria um arquivo com o último frame
+    return faces
 
-webcam.release() # finaliza a conexão com a webcam
-cv2.destroyAllWindows() # garante que as janelas criadas serão fechadas ao encerrar o programa
+def compare_reference_unknown(url_img_reference, encoding_face_unknown):
+    img_reference = fr.load_image_file(url_img_reference)
+    return fr.compare_faces(img_reference, encoding_face_unknown)
+
+def facial_validation(url_img_reference):
+    webcam = cv2.VideoCapture(0) # faz a conexão com a webcam
+
+    ref_img = fr.load_image_file(url_img_reference)
+
+    if webcam.isOpened(): # verifica se a conexão com a webcam foi bem sucedida
+        validacao, frame = webcam.read() # le a imagem da webcam
+
+        while validacao:
+            
+            validacao, frame = webcam.read()
+            cv2.imshow("webcam do joss", frame)
+            
+            # result = fr.compare_faces(ref_img, frame)
+
+            key = cv2.waitKey(3) # retorna um inteiro correpondendo ao código ASCII da tecla apertada
+            
+            if key == 27:
+                break
+
+    webcam.release() # finaliza a conexão com a webcam
+    cv2.destroyAllWindows() # garante que as janelas criadas serão fechadas ao encerrar o programa
+
+
+facial_validation("./images/cr.jpeg")
+
